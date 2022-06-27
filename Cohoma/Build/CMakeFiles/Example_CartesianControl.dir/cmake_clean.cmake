faa591f65d944e14becc07dfe361e406d5a4812a
Betancourt20 <gjc.betancourt@gmail.com>
2022-06-27 22:06:51 +0200

    Mariana caca haha

---
+++ Cohoma/Build/CMakeFiles/Example_CartesianControl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Example_CartesianControl.dir/Example_CartesianControl/src/Example_CartesianControl.cpp.o"
+  "CMakeFiles/Example_CartesianControl.dir/Example_CartesianControl/src/pid.cpp.o"
+  "Example_CartesianControl"
+  "Example_CartesianControl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Example_CartesianControl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Cohoma/Build/CMakeFiles/Test_CartesianOL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test_CartesianOL.dir/Traj_CartesianOL/src/Test_TrajOL.cpp.o"
+  "Test_CartesianOL"
+  "Test_CartesianOL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test_CartesianOL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
